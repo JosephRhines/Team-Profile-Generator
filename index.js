f356240d24d3a08e19fed2b4813c2f8376b82698
Joseph Rhines <jprhines@gmail.com>
2021-01-29 16:01:54 -0600

    I created js files for Employee, Engineer, intern, and manager js files and created classes for Employee and Engineer

---
+++ index.js
@@ -0,0 +1,7 @@
+//questions for employee 
+// .then >> create Employee object
+//type of employee:
+//if manager >> create Manager Object
+// else intern >> create Intern Object
+
+//write file with objects you created.
