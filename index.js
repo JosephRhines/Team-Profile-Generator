a1f6bc54c4d6a90ab2bed64581deace89d634b1c
Joseph Rhines <jprhines@gmail.com>
2021-01-30 18:15:48 -0600

    I added a prompt to the index.html file and classes to each js file. I also corrected some typos in the page-template file

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is your manager name'
+    },
+
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is your manager id'
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address'
+    },
+    
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is your Office Number'
+    }
+
+
+])
 //questions for employee 
 // .then >> create Employee object
 //type of employee:
